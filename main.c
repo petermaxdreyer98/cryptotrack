45fa652e9082cbfd1e9a8ce2787129614f72dc20
Peter Max Dreyer <petermaxdreyer98@gmail.com>
2018-05-08 22:06:26 -0500

    Added in support for selecting specific currencies.

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 #include <curl/curl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define CRYPTOCOMPARE_SINGLE_PRICE "https://min-api.cryptocompare.com/data/price"
 #define CRYPTOCOMPARE_MULTI_PRICE "https://min-api.cryptocompare.com/data/pricemulti"
 
+#define COLOR_RED "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_RESET "\033[37;0m"
+
 struct ResponseData {
 				char* htmlData;
+};
+
+struct CLIArgs{
+				short flags;
+				short currencyCount;
+				char** currencies;
 };
 
 float getCurrencyPrice(char* tickers[], short tickerCount);
@@ -15,10 +26,11 @@ size_t handleRemoteResponse(char *ptr, size_t size, size_t nmemb, void* userdata
 char* getJSONLevel(char* jsonString, char* key);
 char* cleanupJSONTier(char* tier);
 char* retrieveCurrentPrice(char* data, char* ticker);
+struct CLIArgs* doArgs(int argc, char** argv);
 
 int main(int argc, char** argv){
-				char* tickers[] = {"ETH", "NANO", "LTC", "BTC", "XMR", "XRB", "ADA"};
-				float ethPrice = getCurrencyPrice(tickers, 7);
+				struct CLIArgs* cliArgs = doArgs(argc, argv);
+				float ethPrice = getCurrencyPrice(cliArgs->currencies, cliArgs->currencyCount);
 				return 0;
 }
 
@@ -52,10 +64,7 @@ float getCurrencyPrice(char* tickers[], short tickerCount){
 								// Now generate the request URL for libcurl.
 								int requestURLLength = strlen(CRYPTOCOMPARE_MULTI_PRICE) + strlen("?fsyms=") + strlen(currencyList) + strlen("&tsyms=USD\0");
 								char* requestURL = (char*)malloc(sizeof(char) * requestURLLength);
-								strcpy(requestURL, CRYPTOCOMPARE_MULTI_PRICE);
-								strcat(requestURL, "?fsyms=");
-								strcat(requestURL, currencyList);
-								strcat(requestURL, "&tsyms=USD\0");
+								sprintf(requestURL, "%s?fsyms=%s&tsyms=USD\0", CRYPTOCOMPARE_MULTI_PRICE, currencyList);
 								curl_easy_setopt(curl, CURLOPT_URL, requestURL);
 				}
 				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseData);
@@ -109,4 +118,74 @@ char* retrieveCurrentPrice(char* data, char* ticker){
 				char* usd = getJSONLevel(thisStart, "USD");
 				usd = cleanupJSONTier(usd);
 				return usd;
+}
+
+struct CLIArgs* doArgs(int argc, char** argv){
+				// Parse the flags given to the software on the commandline.
+				struct CLIArgs* returnArgs = (struct CLIArgs*)malloc(sizeof(struct CLIArgs));
+				if(argc == 1){ // No arguments were specified.
+								return NULL;
+				}
+				// Arguments have been specified.  Process them.
+				for(int c=1;c!=argc;c++){
+								char* needle = (char*)malloc(sizeof(char)*2);
+								needle = "-f";
+								if(strstr(argv[c], needle)){
+												// The currency flag has been specified.  Parse out the
+												// list and assign it to the struct.
+												char* listStart = argv[c+1];
+												// We should now be at the list start.
+												// Determine the number of currencies in the list based
+												// on the number of commas within the string.
+												// Generate a 2-dimensional array containing all of the
+												// specified currencies.
+												short numCurrencies = 0;
+												for(short x=0;x!=strlen(listStart);x++){
+																if(listStart[x] == ','){
+																				numCurrencies++;
+																}
+												}
+												numCurrencies++;
+												returnArgs->currencyCount = numCurrencies;
+												// We now have the total number of currencies, allocate
+												// space for them.
+												returnArgs->currencies = (char**)malloc(sizeof(char*)*numCurrencies);
+												// The loop for each character in the list.
+												short rawTickerLen = 0;
+												short currentCount = 0;
+												char* tickerPtr = listStart;
+												for(short lc=0;lc!=strlen(listStart)+1;lc++){
+																// Keep counting until either EOL or the first comma.
+																if(listStart[lc] != ',' && listStart[lc] != 0){
+																				rawTickerLen++;
+																}else{
+																				// Determine the number of spaces.
+																				short numNonSpaces = 0;
+																				for(short v=0;v!=rawTickerLen;v++){
+																								if(tickerPtr[v] != ' '){
+																												numNonSpaces++;
+																								}
+																				}
+																				char* thisString = (char*)malloc(sizeof(char)*numNonSpaces);
+																				short currentPos = 0;
+																				for(short v=0;v!=rawTickerLen;v++){
+																								if(tickerPtr[v] != ' '){
+																												thisString[currentPos] = tickerPtr[v];
+																												currentPos++;
+																								}
+																				}
+																				// Convert the letters to uppercase for
+																				// particularly picky APIs
+																				for(short v=0;v!=numNonSpaces;v++){
+																								thisString[v] = toupper(thisString[v]);
+																				}	
+																				tickerPtr = &listStart[lc]+1;
+																				rawTickerLen = 0;
+																				returnArgs->currencies[currentCount] = thisString;
+																				currentCount++;
+																}
+												}
+								}
+				}
+				return returnArgs;
 }
